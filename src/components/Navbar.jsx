7fb46deae2d1403b4be2b182114a9fd1aa55f949
uiukawser-R <kahmed171280@gmail.com>
2024-07-07 22:35:06 +0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                     <button onClick={() => scrollToSection('skills')} className="hover:text-gray-300 focus:outline-none">Skills</button>
                     <button onClick={() => scrollToSection('education')} className="hover:text-gray-300 focus:outline-none">Education</button>
                     <button onClick={() => scrollToSection('contact')} className="hover:text-gray-300 focus:outline-none">Contact</button>
-                    <a href="https://drive.google.com/file/d/1c64xFlO-gBhXxUVe0WVkCAhHvpvfh6YT/view">Resume</a>
+                    <a href="https://drive.google.com/file/d/1Zh94Ii4Tf2Gjl8OneXf8fS3ZYdhAiRZH/view?usp=sharing">Resume</a>
                 </div>
                 <div className="flex items-center gap-4">
                     <a href="https://github.com/uiukawser-R"><FaGithub className="text-white text-xl hover:text-gray-300" /></a>
@@ -49,7 +49,7 @@ const Navbar = () => {
                 <button onClick={() => scrollToSection('skills')} className="block py-3 px-4 text-white focus:outline-none">Skills</button>
                 <button onClick={() => scrollToSection('education')} className="block py-3 px-4 text-white focus:outline-none">Education</button>
                 <button onClick={() => scrollToSection('contact')} className="block py-3 px-4 text-white focus:outline-none">Contact</button>
-                <a href="https://drive.google.com/file/d/1c64xFlO-gBhXxUVe0WVkCAhHvpvfh6YT/view">Resume</a>
+                <a href="https://drive.google.com/file/d/1Zh94Ii4Tf2Gjl8OneXf8fS3ZYdhAiRZH/view?usp=sharing">Resume</a>
             </div>
         </nav>
     );
